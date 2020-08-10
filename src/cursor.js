0219fe5683a9715aaec0b11297e87dc08afeb318
Justin Dennison <justin1dennison@gmail.com>
2020-08-10 16:12:16 -0400

    completed the cursor type

--- src/cursor.js
+++ src/cursor.js
@@ -1,9 +1,76 @@
-function Cursor(buf) {
-  if (!(this instanceof Cursor)) {
-    return new Cursor(buf);
+// enums
+/**
+ *
+ */
+export const SeekFrom = Object.freeze({
+  ABSOLUTE: 0,
+  RELATIVE: 1,
+  FROM_END: 2,
+})
+
+// errors
+/**
+ *
+ */
+export class CursorError extends Error {}
+CursorError.InvalidReadableObject = new CursorError("Invalid readable object")
+CursorError.InvalidPosition = new CursorError("Position outside of range")
+
+/**
+ * Cursor provides a way to maintain the position of a Readable object
+ */
+class Cursor {
+  constructor(buf) {
+    if (!buf) throw CursorError.InvalidReadableObject
+
+    this.buf = buf
+    this.position = 0
   }
-  this.buf = buf;
-  this.position = 0;
+
+  /**
+   *
+   * @param {number} offset
+   * @param {SeekFrom} from
+   */
+  seek(offset, from = SeekFrom.ABSOLUTE) {
+    if (from === SeekFrom.ABSOLUTE && offset > this.buf.length)
+      throw CursorError.InvalidPosition
+    if (from === SeekFrom.FROM_END && this.buf.length - offset < 0)
+      throw CursorError.InvalidPosition
+    if (
+      from === SeekFrom.RELATIVE &&
+        (this.position + offset > this.buf.length ||
+      this.position + offset < 0)
+    )
+      throw CursorError.InvalidPosition
+
+    switch (from) {
+      case SeekFrom.FROM_END:
+        this.position = this.buf.length - offset
+        break
+      case SeekFrom.RELATIVE:
+        this.position += offset
+        break
+      default:
+        this.position = offset
+    }
+  }
+
+  /**
+   *
+   * @param {number} amount
+   */
+  forward(amount) {
+    this.seek(amount, SeekFrom.RELATIVE)
+  }
+
+  /**
+   *
+   * @param {number} amount
+   */
+  rewind(amount) {
+    this.forward(-amount)
+  }
 }
 
-export default Cursor;
+export default Cursor
--- src/cursor.test.js
+++ src/cursor.test.js
@@ -1,8 +1,66 @@
-import test from 'ava'
-import Cursor from './cursor'
+import test from "ava"
+import Cursor, { SeekFrom, CursorError } from "./cursor"
 
+test("cursor is constructable", (t) => {
+  const cursor = new Cursor(Buffer.from([1, 2, 3]))
+  t.assert(cursor, "Cursor is constructable")
+})
 
-test('test cursor is constructable', t => {
-  const cursor = new Cursor()
-  t.assert(cursor, 'Cursor is constructable')
+test("cursor throws when not supplied a valid buffer-like object ", async (t) => {
+  const error = await t.throwsAsync(async () => {
+    const c = new Cursor()
+    return c
+  })
+  t.is(
+    error.message,
+    "Invalid readable object",
+    "You must construct a Cursor with a valid readable object"
+  )
+  t.assert(error instanceof CursorError)
+})
+
+test("cursor cannot seek to a position outside of the range of the inner buffer", async (t) => {
+  const buffer = Buffer.from([1, 2, 3])
+  const cursor = new Cursor(buffer)
+  const error = await t.throwsAsync(async () => {
+    cursor.seek(30)
+  })
+  t.is(
+    error.message,
+    "Position outside of range",
+    "You must seek to valid position within the readable object"
+  )
+  t.assert(error instanceof CursorError)
+})
+
+test("a cursor can seek absolutely", (t) => {
+  const buffer = Buffer.from("abc")
+  const cursor = new Cursor(buffer)
+  t.is(cursor.position, 0, "cursor starts at 0")
+  cursor.seek(2)
+  t.is(cursor.position, 2, "cursor is at position 2")
+})
+
+test("a cursor can seek from the end", (t) => {
+  const buffer = Buffer.from("abcd")
+  const cursor = new Cursor(buffer)
+  t.is(cursor.position, 0, "cursor is at the beginning")
+  cursor.seek(1, SeekFrom.FROM_END)
+  t.is(cursor.position, 3, "cursor is 1 position from the end")
+})
+
+test("a cursor can be rewound", (t) => {
+  const cursor = new Cursor(Buffer.from([1, 2, 3]))
+  cursor.seek(3)
+  t.is(cursor.position, 3, "cursor is at position 3")
+  cursor.rewind(2)
+  t.is(cursor.position, 1, "cursor is at position 1")
+})
+
+test("a cursor can be moved forward", (t) => {
+  const cursor = new Cursor(Buffer.from([1, 2, 3]))
+  cursor.seek(1)
+  t.is(cursor.position, 1, "cursor is at position 1")
+  cursor.forward(2)
+  t.is(cursor.position, 3, "cursor is at position 3")
 })
